5b3ccb586a05f27f5516dc5fcf82ed2138a1bb0a
Matthew Campbell <matjocampbell2@gmail.com>
2024-07-09 10:30:44 -0600

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Matt Campbell on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
